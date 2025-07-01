9a98fd4f1ea2f1bf4389fbfa471b33ff54f0d1ef
superguru <superguru@users.noreply.github.com>
2025-07-02 10:44:11 +1200

    Added example of using HarmonyPrefix instead of a transpiler

--- Harmony/Game/Utils/Game_Utils_Patches.cs
+++ Harmony/Game/Utils/Game_Utils_Patches.cs
@@ -15,12 +15,43 @@ namespace NoBloodMoons.Game.Utils
 #if DEBUG
         [HarmonyDebug]
 #endif
-        private static IEnumerable<CodeInstruction> GameUtils_IsBloodMoonTime_Patch(IEnumerable<CodeInstruction> instructions)
+        private static IEnumerable<CodeInstruction> GameUtils_IsBloodMoonTime_Prime_Patch(IEnumerable<CodeInstruction> instructions)
         {
-            LogUtil.Info("Transpiler: IsBloodMoonTime always returns false.");
+            // Patch is named Prime because it's the only one needed to disable blood moons.
+            // The other method, for overloaded signatures, is not just patched for demonstration purposes.
+
+            LogUtil.Info("Transpiler: IsBloodMoonTime.Prime always returns false.");
             // Replace method body with 'return false;'
             yield return new CodeInstruction(OpCodes.Ldc_I4_0);
             yield return new CodeInstruction(OpCodes.Ret);
+        }
+
+        [HarmonyTranspiler]
+        //public static bool IsBloodMoonTime(ulong _worldTime, (int duskHour, int dawnHour) _duskDawnTimes, int _bmDay)
+        [HarmonyPatch(nameof(GameUtils.IsBloodMoonTime), [typeof(ulong), typeof(ValueTuple<int, int>), typeof(int)])]
+#if DEBUG
+        [HarmonyDebug]
+#endif
+        private static IEnumerable<CodeInstruction> GameUtils_IsBloodMoonTime_Overload_Patch(IEnumerable<CodeInstruction> instructions)
+        {
+            // The Prime method patch is the only one needed for this to work, and this one is just patched for demonstration purposes.
+
+            LogUtil.Info("Transpiler: IsBloodMoonTime.Overload always returns false.");
+            // Replace method body with 'return false;'
+            yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+            yield return new CodeInstruction(OpCodes.Ret);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(GameUtils.IsBloodMoonTime), [typeof(ulong), typeof(ValueTuple<int, int>), typeof(int)])]
+#if DEBUG
+        [HarmonyDebug]
+#endif
+        private static bool Prefix_IsBloodMoonTime_Overload(ref bool __result)
+        {
+            LogUtil.DebugLog("Prefix: IsBloodMoonTime.Overload always returns false.");
+            __result = false;
+            return false; // Skip original method
         }
     }
 }
