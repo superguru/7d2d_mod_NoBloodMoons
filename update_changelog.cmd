ce79f15204fc9a7f37ffdd4a939d219ab88c64b9
superguru <superguru@users.noreply.github.com>
2025-07-07 13:15:28 +1200

    Update update_changelog.cmd

--- update_changelog.cmd
+++ update_changelog.cmd
@@ -1,1 +1,15 @@
-@git-changelog -io NoBloodMoons\CHANGELOG.md 
+@echo off
+
+set PROJECT=NoBloodMoons
+set CHANGELOG_FILE=%PROJECT%\CHANGELOG.md
+set MODINFO_FILE=%PROJECT%\ModInfo.xml
+
+if not exist %CHANGELOG_FILE% (
+set input=
+) else (
+set input=-i
+)
+
+git-changelog %input% -o %CHANGELOG_FILE%
+
+@rem git-changelog --bump 2.0.0 -io NoBloodMoons\CHANGELOG.md 
